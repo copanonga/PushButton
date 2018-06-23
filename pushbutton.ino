40a1f096ce045a51ee2cdfa1eb6eb75144a9e70e
Jose <josemsg@gmail.com>
2018-06-23 09:53:10 +0200

    Add code

--- pushbutton.ino
+++ pushbutton.ino
@@ -1,7 +1,30 @@
+int pushButton = 7;
+int ledPin = 13;
+
 void setup(){
+
+	Serial.begin(9600);
+	pinMode(ledPin, OUTPUT);
+	pinMode(pushButton,INPUT);
   
 }
 
 void loop(){
+
+	Serial.println( );
+
+	if(digitalRead(pushButton) == HIGH) {
+      
+	  	Serial.println("Button 1");
+	    digitalWrite(ledPin, LOW);
+      
+	} else {
+
+		Serial.println("Button 0");
+		digitalWrite(ledPin, HIGH);
+      
+	} 
+  
+  delay(200);
 
 }
